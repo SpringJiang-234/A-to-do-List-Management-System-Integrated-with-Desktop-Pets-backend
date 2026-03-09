c1a285ed2dd624c1caececaaa614d4247798a1bf
winxp <2947602074@qq.com>
2026-03-10 05:01:52 +0800

    feat(domain): 在ClientAnnouncementVO中添加content字段
    为公告信息增加内容字段，以支持存储公告的详细内容

--- src/main/java/com/backend/domain/vo/ClientAnnouncementVO.java
+++ src/main/java/com/backend/domain/vo/ClientAnnouncementVO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ClientAnnouncementVO extends BaseVO {
     private String title;
+    private String content;
     private LocalDateTime updateTime;
     private String isTop;
 }
